a74f640669caf1b081f7cd88fede3331e5104e31
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-06-07 18:37:06 -0700

    group project 1

--- group1.rb
+++ group1.rb
@@ -1,11 +1,6 @@
-fav = []
-p “Please enter your 5 favorite foods!”
-5.times do
-  food = gets.chomp
-  fav << food
-end
-x = 1
-fav.each do |food|
-  p “#{x}. #{food}”
-  x += 1
+count = 0
+
+while count <= 10
+  p count
+  count += 1
 end
